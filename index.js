8f55a72d140f4c8a5181e1be048c65f7edf5a4b0
bath-tub <mbath@payitgov.com>
2023-10-18 23:11:07 -0500

    add receipt id creation

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const express = require('express');
+import { v4 as uuidv4 } from 'uuid';
+import express from 'express';
 const app = express();
 app.use(express.json()); // for parsing application/json
+
+// START SERVER
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
+
+app.set('json spaces', 2); // set number of spaces for indentation
+
 
 let receipts = []; // could change to hashmap?
 
 // ENDPOINTS
-// Create a new item
-app.post('/api/items', (req, res) => {
-    const newItem = req.body;
-    dataStore.push(newItem);
-    res.status(201).send(newItem);
-  });
-  
-// Ping endpoint
-app.get('/api/ping', (req, res) => {
-    res.status(200).send("pong");
+
+// ping pong
+app.get('/ping', (req, res) => {
+  res.status(200).send("pong");
 })
 
-// START SERVER
-app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
-  });
+// Create a new item
+app.post('/receipts/process', (req, res) => {
+    let myuuid = uuidv4();
+    let receipt = { "id" : myuuid };
+    receipts.push(receipt);
+
+    res.status(201).json(receipt);
+});
