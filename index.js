2e97731fd34b13ac99e56332518f66c29d1182db
bath-tub <mbath@payitgov.com>
2023-10-22 10:55:21 -0500

    working post, add git ignore

--- index.js
+++ index.js
@@ -2,29 +2,30 @@ import { v4 as uuidv4 } from 'uuid';
 import express from 'express';
 const app = express();
 app.use(express.json()); // for parsing application/json
+app.set('json spaces', 2); // set number of spaces for indentation
+
+let receipts = new Map(); // could change to hashmap?
 
 // START SERVER
 app.listen(3000, () => {
   console.log('Server running at http://localhost:3000/');
 });
 
-app.set('json spaces', 2); // set number of spaces for indentation
-
-
-let receipts = []; // could change to hashmap?
-
 // ENDPOINTS
-
-// ping pong
+// ping endpoint to test if server is running
 app.get('/ping', (req, res) => {
   res.status(200).send("pong");
 })
 
-// Create a new item
+// Process a receipt
 app.post('/receipts/process', (req, res) => {
-    let myuuid = uuidv4();
-    let receipt = { "id" : myuuid };
-    receipts.push(receipt);
-
-    res.status(201).json(receipt);
+    let uuid = uuidv4();
+    let payload = req.body;
+    console.log(payload);
+    receipts.set(uuid, payload);
+    
+    let response = { "id" : uuid };
+    res.status(201).json(response);
 });
+
+// Get points for a receipt
